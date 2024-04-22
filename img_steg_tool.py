e7f83a8c8242b0b1085f7d6dd34f746dc0d3b7bd
WoaW04 <100178888+WoaW04@users.noreply.github.com>
2024-04-22 23:25:14 +0800

    更新图片显示逻辑
    显示的图片默认居中，超出大小可用滚动条滚动

--- img_steg_tool.py
+++ img_steg_tool.py
@@ -1,8 +1,9 @@
 import numpy as np
 import PIL.Image as Image
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import *
 import cv2
 import sys
 from utils.watermark import *
@@ -36,13 +37,20 @@ class Ui(QtWidgets.QMainWindow):
 
     def ReadImg(self, path):
         self.src = np.array(Image.open(path))
-        # self.ui.ShowImgLabel.setPixmap(ToPixmap_1(self.src))
-        # pix = QPixmap(path)
-        # pix = QPixmap.fromImage(Image.fromarray(cv2.cvtColor(self.src, cv2.COLOR_RGBA2BGRA)).convert('RGB'))
-        self.ui.ShowImgLabel.setPixmap(ToPixmap(self.src))
-
+        self.ShowImg()
         print("图像信息：")
         print(f"长：{self.src.shape[1]}\t宽：{self.src.shape[0]}\t通道数：{self.src.shape[2]}")
+
+    def ShowImg(self):
+        self.ShowImgLabel = QLabel()
+        self.ShowImgLabel.setPixmap(ToPixmap(self.src))
+        self.scroll = QScrollArea()
+        self.scroll.setAlignment(Qt.AlignCenter)
+        self.scroll.setFrameShape(QFrame.NoFrame)
+        self.scroll.setWidget(self.ui.ShowImgLabel)
+        self.mainLayout = QGridLayout()
+        self.mainLayout.addWidget(self.scroll, 0, 0)
+        self.ui.ShowImgWidget.setLayout(self.mainLayout)
 
     def saveimg(self):
         Img = self.src
