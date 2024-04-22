45efe29da77443d2310a9ce768f9e09dd4329eed
NgIokWeng <2334439391@qq.com>
2024-04-22 10:26:50 +0800

    簡單的盲水印測試

--- img_steg_tool.py
+++ img_steg_tool.py
@@ -1,5 +1,6 @@
 import numpy as np
 import PIL.Image as Image
+from utils.watermark import *
 
 
 def ReadImg(src, path):
@@ -15,3 +16,10 @@ if __name__ == '__main__':
     src = np.array(0)
     ReadImg(src, jpeg_test_path)
     ReadImg(src, png_test_path)
+
+    len_wm1 = embedBlindWatermark(jpeg_test_path, "jpg_embed_test.jpg", "Hello World1")
+    extractBlindWatermark("jpg_embed_test.jpg", len_wm1)
+
+    len_wm2 = embedBlindWatermark(png_test_path, "png_embed_test.jpg", "Hello World2")
+    extractBlindWatermark("png_embed_test.jpg", len_wm2)
+
