fd71560d96edbc0733272c86caf8d1de1e999b01
WoaW04 <100178888+WoaW04@users.noreply.github.com>
2024-04-22 11:15:36 +0800

    OpenImg
    接下来需要让label显示图片

--- img_steg_tool.py
+++ img_steg_tool.py
@@ -1,17 +1,52 @@
 import numpy as np
 import PIL.Image as Image
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QFileDialog
+import cv2
+import sys
 
 
-def ReadImg(src, path):
-    src = np.array(Image.open(path))
+def ToPixmap_1(arr):  # arr对应四通道图片。额外使用PIL.Image模块
+    # https://blog.csdn.net/ielcome2016/article/details/105798279
+    from PIL import Image
+    arr = cv2.cvtColor(arr, cv2.COLOR_RGBA2BGRA)
+    return Image.fromarray(arr).toqpixmap()
 
-    print("图像信息：")
-    print(f"长：{src.shape[1]}\t宽：{src.shape[0]}\t通道数：{src.shape[2]}")
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi("mainwindow.ui", self)
+        self.ImgPath = ''
+        self.src = np.array(0)
+        self.InitUI()
+
+    def InitUI(self):
+        self.ui.OpenImg.clicked.connect(self.openimg)
+        self.ui.SaveCurrentImg.clicked.connect(self.saveimg)
+
+    def openimg(self):
+        self.ImgPath, _ = QFileDialog.getOpenFileName(self.centralwidget, "选择图片",
+                                                      "./", "Image files (*.jpg *.gif *.png *.jpeg)")
+        self.ReadImg(self.ImgPath)
+
+    def ReadImg(self, path):
+        self.src = np.array(Image.open(path))
+        self.ui.ShowImgLabel.setPixmap(ToPixmap_1(self.src))
+
+        print("图像信息：")
+        print(f"长：{self.src.shape[1]}\t宽：{self.src.shape[0]}\t通道数：{self.src.shape[2]}")
+
+    def saveimg(self):
+        Img = self.src
+        filepath, _ = QFileDialog.getSaveFileName(self, "文件保存", "/", 'Image files (*.jpg *.gif *.png *.jpeg)')
+        file = open(filepath, 'w')
+        print(filepath)
+        file.write(Img)
 
 
 if __name__ == '__main__':
-    jpeg_test_path = "./test.jpg"
-    png_test_path = "./test.png"
-    src = np.array(0)
-    ReadImg(src, jpeg_test_path)
-    ReadImg(src, png_test_path)
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    window.show()
+    sys.exit(app.exec_())
